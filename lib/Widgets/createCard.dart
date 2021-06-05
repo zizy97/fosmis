6bbb4242b6801f9081301390b18492288f7541e0
zizy97 <supuntharuka9749@gmail.com>
2021-06-06 03:52:15 +0530

    notification icon set

--- lib/Widgets/createCard.dart
+++ lib/Widgets/createCard.dart
@@ -1,12 +1,16 @@
 // import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:fosmis/model/newsdata.dart';
+import 'package:intl/intl.dart';
 // import 'package:fosmis/pages/dataview.dart';
 // import 'package:fosmis/pages/home.dart';
 
 Widget createCard(
     NewsDatum newsdata, BuildContext context, VoidCallback opencontainer) {
-  String date = newsdata.date.split("/")[0];
+  //DateTime date = DateTime.parse(newsdata.date);
+  // DateFormat df = DateFormat('dd-MM-yyyy hh:mm');
+  // String f_date = df.format(date);
+
   return GestureDetector(
     onTap: opencontainer,
     child: Card(
@@ -20,11 +24,13 @@ Widget createCard(
         child: Column(
           children: [
             ListTile(
-              leading: Icon(Icons.arrow_drop_down_circle),
+              leading: Icon(newsdata.recent
+                  ? Icons.notifications_active_outlined
+                  : Icons.circle_notifications),
               title: Text(newsdata.title,
                   style: TextStyle(fontWeight: FontWeight.bold)),
               subtitle: Text(
-                date,
+                newsdata.date,
                 style: TextStyle(color: Colors.black.withOpacity(0.6)),
               ),
             ),
